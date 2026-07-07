e76f2c07db72a200a3ab973907f0bc72972075b9
AdityaKumar10805 <adityak.ug24.cs@nitp.ac.in>
2026-07-07 15:21:40 +0530

    Allow Vercel frontend

--- server/app.js
+++ server/app.js
@@ -5,11 +5,14 @@ const resumeRoutes = require("./routes/resumeRoutes");
 const aiRoutes = require("./routes/aiRoutes");
 const app = express();
 app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://career-forge-fsam-9hdt0oc5p-single9.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 // Middleware
 app.use(express.json());
