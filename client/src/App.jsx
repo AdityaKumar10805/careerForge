1ec71ce683b662b81c7be8d532da7b5dee93d3dd
AdityaKumar10805 <adityak.ug24.cs@nitp.ac.in>
2026-07-07 14:59:28 +0530

    Fix Login import case for Vercel deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ResumeBuilder from "./pages/ResumeBuilder";
